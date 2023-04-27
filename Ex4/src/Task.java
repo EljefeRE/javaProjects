2368ce42e262d6ccfb4de1df900c2012aefd4f02
Sergey <isergeytarhov@gmail.com>
2023-04-27 11:28:42 +0300

    add Task1

---
+++ Ex4/src/Task.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+/*
+1. Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+Постараться не обращаться к листу по индексам.
+3. Найдите сумму всех элементов LinkedList, сохраняя все элементы в списке. Используйте итератор
+ */
+public class Task {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+        LinkedList<Integer> newList = new LinkedList<>();
+        ListIterator<Integer> listIterator = list.listIterator(list.size());
+        System.out.println(list);
+
+        while (listIterator.hasPrevious()) {
+            newList.add(listIterator.previous());
+        }
+
+        System.out.println(newList);
+    }
+}
